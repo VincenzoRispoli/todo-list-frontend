789585a0281c90a860b106b65e0fa11d6a04405d
VincenzoRispoli <150629139+VincenzoRispoli@users.noreply.github.com>
2024-09-28 15:41:33 +0200

    new commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  email:string = "";
+  password:string = ""
+
+  login(){
+    // logik, um mit dem Backend zu kommunizieren
+  }
+
+}
